ef7f57a7176e17ed67574d95dc8a9972da54f11d
Ben Gamari <ben@smart-cactus.org>
2019-07-17 21:22:49 -0400

    nix: Add ARM gdb

--- firmware/default.nix
+++ firmware/default.nix
@@ -1,5 +1,12 @@
-let nixpkgs = import ~/.nix-overlay/nixpkgs {};
-in with nixpkgs; stdenv.mkDerivation {
+let nixpkgs = import <nixpkgs> {};
+in with nixpkgs;
+let
+  gdb = nixpkgs.gdb.overrideAttrs (oldAttrs: {
+    configureFlags = [ "--target=armv6-none-eabi" ];
+  });
+
+env = stdenv.mkDerivation {
   name = "firmware";
-  nativeBuildInputs = [ gcc-arm-embedded gnumake openocd ];
-}
+  nativeBuildInputs = [ gcc-arm-embedded gnumake openocd gdb ];
+};
+in env
